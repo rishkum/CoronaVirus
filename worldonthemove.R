3b1787484dc2af80457e591b5e38bae88e8dc1af
rishkum <kumarrishabh14@hotmail.com>
2020-05-17 20:41:31 +0100

    Code re written with better presentation

--- worldonthemove.R
+++ worldonthemove.R
@@ -1,4 +1,30 @@
-#On the move
+#World on the move
+
+# The aim to find out how different movement ways have changed across countries
+# We should have 3 different regions
+# World, UK, US 
+# p is for plot 
+# m if for models
+# tvl, vst, fcst
+#
+# The questions that we want to answer
+# 1. Are people traveling more and then what ways? - Apple Mobility Data 
+# S: Change from baseline in a graph and numbers
+# M: Plots x 3,  Change in absolute numbers before lockdown and now
+# A: T
+# R: T
+# T: 1hr
+#
+# 2. Where are they going? - Google Mobility Data
+# S: Change from baseline in a graph and numbers
+# M: Plots x 3,  Change in absolute numbers before lockdown and now
+# A: T
+# R: T
+# T: 1hr
+
+# 3. Where are we heading
+# 
+
 library(tidyverse)
 dataApple <- read.csv("data/applemobilitytrends-2020-05-14.csv")
 policy <- read.csv("data/policy.csv")
@@ -6,9 +32,7 @@ gdata <- read.csv("data/google_mobility_Data.csv")
 
 
 df<- dataApple
-
-
-#Start here
+######## Start here ############
 #Overall trend
 
 df <- df %>% select(- c(geo_type , alternative_name)) %>% pivot_longer(- c(region, transportation_type), names_to = "date", values_to = "value")
@@ -21,11 +45,12 @@ df$date <- as.Date(df$date)
 
 gdata$date <- as.Date(gdata$date)
 
-#basic geom
+# basic geom ####
 
 ggplot(df, aes(date, value)) + geom_line() + geom_smooth()
 
-# Get continent level data for multiple line chart
+# Get continent level data for multiple line chart####
+
 library(countrycode)
 df$continent <- countrycode(sourcevar = df$region,
                             origin = "country.name",
@@ -33,54 +58,152 @@ df$continent <- countrycode(sourcevar = df$region,
 
 ggplot(df, aes(date, value, color = continent)) + geom_line() + geom_smooth()
 
-#Checking for UK
-df %>% filter(region == "UK") %>%   
-  ggplot( ., aes(date, value, color = transportation_type)) + geom_line() +
-  geom_smooth() +
-  geom_vline(xintercept = as.Date(c("2020-03-23", "2020-05-13") )) + ggpretty()
 
-df %>% filter(region == "Germany") %>%   
-  ggplot( ., aes(date, value, color = transportation_type)) + geom_line() +
-  geom_smooth() +
-  geom_vline(xintercept = as.Date(c("2020-03-23", "2020-05-13") )) + ggpretty()
+####### cleaning data sets #########
+df <-   df %>% mutate(transportation_type = recode(transportation_type, 
+                                                                'driving' = 'Driving',
+                                                                'transit' = 'Transit',
+                                                                'walking' = 'Walking'))
 
+gdata <- gdata %>% mutate(transportation_type = recode(move_to, 
+                                                       'retail_and_recreation_percent_change_from_baseline' = 'Retail and recreation',
+                                                       'grocery_and_pharmacy_percent_change_from_baseline' = 'Grocery & Essentials',
+                                                       'transit_stations_percent_change_from_baseline' = 'Public Trasport',
+                                                        'workplaces_percent_change_from_baseline' = 'Workplace',
+                                                       'parks_percent_change_from_baseline'       = 'Parks',
+                                                         'residential_percent_change_from_baseline' = 'Home'))
 
-df %>% filter(region == "Sweden") %>%   
-  ggplot( ., aes(date, value, color = transportation_type)) + geom_line() +
-  geom_smooth() +
-  geom_vline(xintercept = as.Date(c("2020-03-23", "2020-05-13") )) 
+####### 1. Are people traveling more and then what ways? - Apple Mobility Data #########
 
+# Checking for UK 
 
+p.tvl.uk <- df %>% filter(region == "UK") %>%   
+  ggplot( ., aes(as.Date(date), value, color = transportation_type)) + geom_line() +
+  geom_smooth() + scale_y_continuous(labels = function(x) paste0(x-100, "%")) +
+  scale_colour_brewer(palette = "Set2") +
+  guides(color=guide_legend(override.aes=list(fill=NA)))
 
-#Gdata
-gdata %>% filter(country_region == "Sweden") %>%   
-  ggplot( ., aes(date, value, color = move_to)) + geom_line() +
-  geom_smooth() +
-  geom_vline(xintercept = as.Date(c("2020-03-23", "2020-05-13") )) 
-
-gdata %>% filter(country_region == "United Kingdom") %>%   
-  ggplot( ., aes(date, value, color = move_to)) + geom_line() +
-  geom_smooth() +
-  geom_vline(xintercept = as.Date(c("2020-03-23", "2020-05-13") )) 
-
-
-gdata %>% filter(country_region == "United States") %>%   
-  ggplot( ., aes(date, value, color = move_to)) + geom_line() +
-  geom_smooth() +
-  geom_vline(xintercept = as.Date(c("2020-03-23", "2020-05-13") )) 
-
-
-gdata %>% filter(country_region == "India") %>%   
-  ggplot( ., aes(date, value, color = move_to)) + geom_line() +
-    geom_smooth(method = "loess") +
-  geom_vline(xintercept = as.Date(c("2020-03-23", "2020-05-13") )) + ggpretty()
-
-gdata %>% 
-  ggplot( ., aes(date, value, color = move_to))  +
-  geom_smooth() +
-  geom_vline(xintercept = as.Date(c("2020-03-23", "2020-05-13") ))
-
-+ ggpretty()
+# Annote it
+p.tvl.uk <- ggpretay(p.tvl.uk, title = "Is Britain on the move again?", 
+         subtitle = "Data from Apple's mobility data suggests that people have started to travel again.
+It does seem like people are shunning public transport usage is recovering at a lower pace.",
+        xaxis = "Months", yaxis = "Change from baseline"  )
+#Save the plot
+finalise_plot(p.tvl.uk, source_name = "Source:Apple Mobility Dataset", 
+              save_filepath = "Plots/On_the_move/tvl_uk.png",
+              logo_image_path = "Data/logo/rk_logo.png")
 
 
-temp <- policy %>% filter(CountryName == "United Kingdom" & C4_Restrictions.on.gatherings == 4)
+# Checking for US 
+
+p.tvl.us <- df %>% filter(region == "United States") %>%   
+  ggplot( ., aes(as.Date(date), value, color = transportation_type)) + geom_line() +
+  geom_smooth() + scale_y_continuous(labels = function(x) paste0(x-100, "%")) +
+  scale_colour_brewer(palette = "Set2") +
+  guides(color=guide_legend(override.aes=list(fill=NA))) 
+
+# Preety fy the graph
+p.tvl.us <- ggpretay(p.tvl.uk, title = "US seems to be on the move too", 
+                     subtitle = "Data from Apple's mobility data suggests that Americans have started to travel again.
+Like in Britain, Americans are shunning public transport usage",
+                     xaxis = "Months", yaxis = "Change from baseline"  )
+
+# Annote the graph
+p.tvl.usa <- p.tvl.us +
+  #Adding vline
+  geom_vline(xintercept = as.Date("2020-01-30"), 
+             color = "dark gray", linetype = "dashed", show.legend = T) +
+  #Curve
+  geom_curve(curvature = -0.15, color = "dark gray",
+             # Position , no quotes cause continous
+             #0.03 is the size, curvature is the shape
+             aes(x = as.Date("2020-01-30"), y = 160.00, xend = as.Date("2020-04-15"), yend = 160.00),
+             arrow = arrow(length = unit(0.02, "npc"))) + 
+  # text
+  annotate("text", x = as.Date("2020-04-30"),
+           y = 150,
+           label = "WHO declares\ncoronavirus a pandemic",
+           size = 3.5, color = "dark gray") 
+
+  
+
+#Save the plot
+finalise_plot(p.tvl.usa, source_name = "Source:Apple Mobility Dataset", 
+              save_filepath = "Plots/On_the_move/tvl_us.png",
+              logo_image_path = "Data/logo/rk_logo.png")
+
+
+# Checking for World 
+p.tvl.wld <- df %>%   
+  ggplot( ., aes(as.Date(date), value, color = transportation_type)) + geom_line() +
+  geom_smooth() + scale_y_continuous(labels = function(x) paste0(x-100, "%")) +
+  scale_colour_brewer(palette = "Set2") +
+  guides(color=guide_legend(override.aes=list(fill=NA))) 
+
+# Preety fy the graph
+p.tvl.wld <- ggpretay(p.tvl.uk, title = "World is getting back on the move", 
+                     subtitle = "Data suggests that globally people have started to travel again.
+And the general trend is about shunning public transport",
+                     xaxis = "Months", yaxis = "Change from baseline"  )
+
+# Annote the graph
+p.tvl.wld1 <- p.tvl.wld +
+  #Adding vline
+  geom_vline(xintercept = as.Date("2020-01-30"), 
+             color = "dark gray", linetype = "dashed", show.legend = T) +
+  #Curve
+  geom_curve(curvature = -0.15, color = "dark gray",
+             # Position , no quotes cause continous
+             #0.03 is the size, curvature is the shape
+             aes(x = as.Date("2020-01-30"), y = 160.00, xend = as.Date("2020-04-15"), yend = 160.00),
+             arrow = arrow(length = unit(0.02, "npc"))) + 
+  # text
+  annotate("text", x = as.Date("2020-04-30"),
+           y = 150,
+           label = "WHO declares\ncoronavirus a pandemic",
+           size = 3.5, color = "dark gray") 
+
+
+#Save the plot
+finalise_plot(p.tvl.wld1, source_name = "Source:Apple Mobility Dataset", 
+              save_filepath = "Plots/On_the_move/tvl_wld.png",
+              logo_image_path = "Data/logo/rk_logo.png")
+
+
+
+######## Where is the world Traveling too ###########
+# Checking for World 
+p.vst.wld <- gdata %>%   
+  ggplot( ., aes(as.Date(date), value, color = move_to)) + 
+  geom_smooth() + scale_y_continuous(labels = function(x) paste0(x, "%")) +
+  scale_colour_brewer(palette = "Set2") +
+  guides(color=guide_legend(override.aes=list(fill=NA))) 
+
+# Preety fy the graph
+p.vst.wld <- ggpretay(p.vst.wld, title = "Let's go to the park", 
+                      subtitle = "Data suggests that globally people have started to travel again.
+And the general trend is about shunning public transport",
+                      xaxis = "Months", yaxis = "Change from baseline"  )
+
+# Annote the graph
+p.vst.wld1 <- p.vst.wld +
+  #Adding vline
+  geom_vline(xintercept = as.Date("2020-01-30"), 
+             color = "dark gray", linetype = "dashed", show.legend = T) +
+  #Curve
+  geom_curve(curvature = -0.15, color = "dark gray",
+             # Position , no quotes cause continous
+             #0.03 is the size, curvature is the shape
+             aes(x = as.Date("2020-01-30"), y = 50, xend = as.Date("2020-03-15"), yend = 160.00),
+             arrow = arrow(length = unit(0.02, "npc"))) + 
+  # text
+  annotate("text", x = as.Date("2020-04-30"),
+           y = 150,
+           label = "WHO declares\ncoronavirus a pandemic",
+           size = 3.5, color = "dark gray") 
+
+
+#Save the plot
+finalise_plot(p.vst.wld1, source_name = "Source:Apple Mobility Dataset", 
+              save_filepath = "Plots/On_the_move/tvl_wld.png",
+              logo_image_path = "Data/logo/rk_logo.png")
